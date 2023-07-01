4f99079a871ee700f05b99370ef2a341b7bc9fc0
leonsound <wind200806@163.com>
2023-07-01 09:10:14 +0800

    新增角色类， 设置网格体， 动画实例

---
+++ Source/ActionRoguelike/Public/Test/MyCharacter.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere) // 暴露组件， 蓝图子类中可以看到并修改
+	class USpringArmComponent* SpringArmComp;
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* CameraComp;
+
+	
+	void MoveForward(float value); // 0~1
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
